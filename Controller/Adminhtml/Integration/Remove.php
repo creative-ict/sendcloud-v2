960dab05c0874ab92e4106b5b77b9dfea1ebd960
Aderik Teekman <a.teekman@creativeict.nl>
2021-02-05 10:14:07 +0100

    SC-46: Change integration flow

---
+++ Controller/Adminhtml/Integration/Remove.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace SendCloud\SendCloudV2\Controller\Adminhtml\Integration;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Integration\Api\IntegrationServiceInterface;
+
+class Remove extends Action
+{
+
+    protected $integrationService;
+
+    public function __construct(
+        Context $context,
+        IntegrationServiceInterface $integrationService
+    )
+    {
+        parent::__construct($context);
+        $this->integrationService = $integrationService;
+    }
+
+    /**
+     * @return ResponseInterface|Redirect|ResultInterface
+     */
+    public function execute()
+    {
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('adminhtml/system_config/edit/', ['section' => 'sendcloudv2']);
+
+        $integration = $this->integrationService->findByName('SendCloud');
+        if(!$integration->getId())
+        {
+            $this->messageManager->addExceptionMessage(__('Integration SendCloud not found'));
+            return $resultRedirect;
+        }
+
+        try{
+            $integration->delete();
+            $this->messageManager->addSuccessMessage(__('SendCloud Integration removed.'));
+        } catch (Exception $e) {
+            $this->messageManager->addExceptionMessage(__('Remove SendCloud Integration fails. ' . $e->getMessage()));
+        }
+
+        return $resultRedirect;
+    }
+}
